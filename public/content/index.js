174274be6e82aabf4d95bd6ad9a20f1cd6dfb34e
Nazim Boudeffa <boudeffa@gmail.com>
2019-09-25 22:26:38 +0200

    Fixed some links

--- public/content/index.js
+++ public/content/index.js
@@ -2,70 +2,70 @@ module.exports = [
   {
     "category" : "tutorial",
     "file" : "part01",
-    "image" : "phaser-logo-750x300",
+    "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
     "category" : "tutorial",
     "file" : "part02",
-    "image" : "phaser-logo-750x300",
+    "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
     "category" : "tutorial",
     "file" : "part03",
-    "image" : "phaser-logo-750x300",
+    "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
     "category" : "tutorial",
     "file" : "part04",
-    "image" : "phaser-logo-750x300",
+    "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
     "category" : "tutorial",
     "file" : "part05",
-    "image" : "phaser-logo-750x300",
+    "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
     "category" : "tutorial",
     "file" : "part06",
-    "image" : "phaser-logo-750x300",
+    "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
     "category" : "tutorial",
     "file" : "part07",
-    "image" : "phaser-logo-750x300",
+    "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
     "category" : "tutorial",
     "file" : "part08",
-    "image" : "phaser-logo-750x300",
+    "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
     "category" : "tutorial",
     "file" : "part09",
-    "image" : "phaser-logo-750x300",
+    "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
     "category" : "tutorial",
     "file" : "part10",
-    "image" : "phaser-logo-750x300",
+    "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   }
