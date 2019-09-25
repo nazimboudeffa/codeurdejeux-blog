dc81c822dc61b9d862de3d332c732a58cc139fbb
Nazim Boudeffa <boudeffa@gmail.com>
2019-09-25 23:06:49 +0200

    Added more items in the menu

--- public/content/index.js
+++ public/content/index.js
@@ -1,69 +1,69 @@
 module.exports = [
   {
-    "category" : "tutorial",
+    "category" : "making-your-first-phaser-3-game",
     "file" : "part01",
     "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
-    "category" : "tutorial",
+    "category" : "making-your-first-phaser-3-game",
     "file" : "part02",
     "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
-    "category" : "tutorial",
+    "category" : "making-your-first-phaser-3-game",
     "file" : "part03",
     "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
-    "category" : "tutorial",
+    "category" : "making-your-first-phaser-3-game",
     "file" : "part04",
     "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
-    "category" : "tutorial",
+    "category" : "making-your-first-phaser-3-game",
     "file" : "part05",
     "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
-    "category" : "tutorial",
+    "category" : "making-your-first-phaser-3-game",
     "file" : "part06",
     "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
-    "category" : "tutorial",
+    "category" : "making-your-first-phaser-3-game",
     "file" : "part07",
     "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
-    "category" : "tutorial",
+    "category" : "making-your-first-phaser-3-game",
     "file" : "part08",
     "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
-    "category" : "tutorial",
+    "category" : "making-your-first-phaser-3-game",
     "file" : "part09",
     "banner" : "phaser-logo-750x300",
     "author" : "admin",
     "date" : "2019-08-10"
   },
   {
-    "category" : "tutorial",
+    "category" : "making-your-first-phaser-3-game",
     "file" : "part10",
     "banner" : "phaser-logo-750x300",
     "author" : "admin",
