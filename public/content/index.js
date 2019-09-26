eacf6ac9611267102c7d5560900d06fc9fd00955
Nazim Boudeffa <boudeffa@gmail.com>
2019-09-26 21:28:42 +0200

    Fixing some typo

--- public/content/index.js
+++ public/content/index.js
@@ -3,7 +3,7 @@ module.exports = [
     "category" : "tutorial",
     "file" : "making-your-first-phaser-3-game",
     "banner" : "phaser-logo-750x300",
-    "title" : "Créer son premier jeu phaser 3",
+    "title" : "Créer son premier jeu Phaser 3",
     "description" : "Nous allons voir comment créer son premier jeu phaser3",
     "author" : "admin",
     "tags" : "phaser3",
